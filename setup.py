2c322eb4c36ff31ba7ca87709fd0f62d6d32d92c
colmjude <colmjude@gmail.com>
2022-07-17 17:46:32 +0100

    Add google fonts to base head

--- setup.py
+++ setup.py
@@ -8,24 +8,19 @@ directories_jinja = glob.glob("colmjude_frontend/**/**/*.jinja", recursive=True)
 
 for directory in directories_jinja:
     components.append(
-        os.path.relpath(os.path.dirname(directory), "colmjude_frontend")
-        + "/*.jinja"
+        os.path.relpath(os.path.dirname(directory), "colmjude_frontend") + "/*.jinja"
     )
 
 setup(
     name="colmjude-frontend",
-    version="0.1",
+    version="0.1.1",
     author="Colm Britton",
     description="Reusable frontend code for colmjude.com",
     license="MIT",
     packages=find_packages(exclude=["tests"]),
     package_data={"colmjude-frontend": components},
     python_requires=">=3.5",
-    install_requires=[
-        "jinja2"
-    ],
+    install_requires=["jinja2"],
     include_package_data=True,
-    extras_require={
-        "testing": ["pytest", "flake8", "black"]
-    }
+    extras_require={"testing": ["pytest", "flake8", "black"]},
 )
